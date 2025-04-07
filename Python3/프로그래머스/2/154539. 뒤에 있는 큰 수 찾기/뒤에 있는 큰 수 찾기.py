c15cb0eb3f2b278083ca27b9503122e638aa2d88
jungeun yoon <angal2310@tukorea.ac.kr>
2025-04-07 14:05:47 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 1772.34 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    answer = [-1]*len(numbers)
+    for i in range(len(numbers)-1,-1,-1):
+        for j in range(i-1,-1,-1):
+            if numbers[j] >= numbers[i]:
+                break
+            answer[j] = numbers[i]
+    return answer
