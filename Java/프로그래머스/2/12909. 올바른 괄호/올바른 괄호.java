7eb3542c27727908de50695b05cf3ddf8584b9d6
jungeun yoon <angal2310@tukorea.ac.kr>
2025-05-01 16:38:06 +0900

    Update 올바른 괄호.java

--- Java/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ Java/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,22 +3,16 @@ import java.util.Stack;
 class Solution {
     boolean solution(String s) {
         Stack<Character> st = new Stack<>();
-        boolean answer = true;
         
         for (char c : s.toCharArray()) {
             if (c=='('){
                 st.push(c);
             } else {
-                if (st.isEmpty()) {
-                    answer = false;
-                } else {
-                    st.pop();
-                }
+                if (st.isEmpty()) {return false;} 
+                st.pop();
             } 
         }
-        if (!st.isEmpty()){
-                answer = false;
-        }
-        return answer;
+        return st.isEmpty();
     }
 }
+
